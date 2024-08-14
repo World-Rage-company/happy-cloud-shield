cbe1ab383a27631767f4f8859e4a59e7a34f73e4
World Rage <163778240+World-Rage-company@users.noreply.github.com>
2024-08-14 21:41:11 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -201,9 +201,16 @@ EOF
 
     sed -i "s/DB_USER', ''/DB_USER', '$mysql_user'/; s/DB_PASS', ''/DB_PASS', '$mysql_pass'/" /var/www/html/happy-cloud-shield/assets/php/database/config.php
 
-    mysql -u "$mysql_user" -p"$mysql_pass" HCS_db <<EOF
+    user_exists=$(mysql -u "$mysql_user" -p"$mysql_pass" HCS_db -se "SELECT COUNT(*) FROM admins WHERE username='admin';")
+
+    if [ "$user_exists" -eq 0 ]; then
+        echo -e "${YELLOW}Admin user does not exist. Creating new admin user...${NC}"
+        mysql -u "$mysql_user" -p"$mysql_pass" HCS_db <<EOF
 INSERT INTO admins (username, password) VALUES ('admin', '$2a$12$OpVPiKsWXY4P3M0RSCOkiuBxOyCG2WIcXljN3J6aF3jKGE5N7oOBC');
 EOF
+    else
+        echo -e "${GREEN}Admin user already exists. Skipping creation.${NC}"
+    fi
 }
 
 finish() {
