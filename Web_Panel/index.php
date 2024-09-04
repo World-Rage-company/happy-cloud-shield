26a064cf25738e9bfc72fab89d4b80386ec9968a
World Rage <163778240+World-Rage-company@users.noreply.github.com>
2024-09-04 22:53:55 +0330

    Update index.php

--- Web_Panel/index.php
+++ Web_Panel/index.php
@@ -103,6 +103,7 @@ if (!isset($_SESSION['admin_id'])) {
                     </div>
                     <div class="home-content">
                         <!-- Home Options -->
+                        <div class=""></div>
                     </div>
                 </div>
             </section>
